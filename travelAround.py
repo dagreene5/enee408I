fb1759c8edd1874f02f87b6a6760fa04af23c71c
davidgreene <dagreene5@gmail.com>
2016-03-22 16:22:53 -0400

    hi

--- travelAround.py
+++ travelAround.py
@@ -135,7 +135,8 @@ while (response == ""):
     response = port.readline();
 
 numAverages = 10;
-movingAverageList = LinkedList(0);
+firstNode = Node(0);
+movingAverageList = LinkedList(firstNode);
 movingSum = 0;
 print("Initializing moving average...\n");
 while (movingAverageList.size() < numAverages):
