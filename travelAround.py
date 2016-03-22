b26de8718033b024cd4d9f53031d0694386bbd5b
davidgreene <dagreene5@gmail.com>
2016-03-22 15:45:15 -0400

    hji

--- travelAround.py
+++ travelAround.py
@@ -106,7 +106,7 @@ while (1):
 
     if (objectDetected(distanceLeft)):
         rotateCounterClockwise();
-    else if (objectDetected(distanceRight)):        
+    elif (objectDetected(distanceRight)):        
         rotateClockwise();
 
     while (objectDetected(distanceLeft) or
