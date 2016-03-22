fff949770308d5795edd18f78a0acb6d365f3285
davidgreene <dagreene5@gmail.com>
2016-03-22 15:55:21 -0400

    shutdown

---
+++ shutdown.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import struct
+import serial
+
+port = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1);   # communication with arduino
+             # for optical input
+
+
+def halt():
+    port.write("he");
+    return;
+
+def getPingLeft():
+    port.write("gple");
+    ret = port.readline();
+    print("ping returned: " + ret);
+    return int(ret);
+
+response = "";
+while (response == ""):
+    port.write("gple");
+    port.flush();
+    response = port.readline();
+
+halt();
+file.close();
