d6741e78060f5e38ef6eafff4325dfad373e257a
davidgreene <dagreene5@gmail.com>
2016-04-26 17:40:05 -0400

    hi

--- searchAndDeliver.py
+++ searchAndDeliver.py
@@ -364,7 +364,7 @@ high_x_bound = 185
 state_searching = 1
 state_delivering = 2
 #min_cone_distance = 10
-min_cone_distance = 11
+min_cone_distance = 9
 min_obstacle_distance = 30
 lastDepositDirection = -1
 
